25eec8828680d242e7225722ad36097aa096a749
Brady Stephenson <bstephenson@cardinalsolutions.com>
2016-04-12 20:34:37 -0400

    Forgot to save recent changes in sublime, Intellij making me lazy

---
+++ app/br-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component } from 'angular2/core';
+import { Beer } from './beer';
+
+@Component({
+  selector: 'br-detail',
+  inputs: ['beer'],
+  template: `
+    <div class="beer-item">
+      <div class="name">Name: {{beer.name}}</div>
+      <div class="brewery">Brewery: {{beer.brewery}}</div>
+      <div class="type">Type: {{beer.type}}</div>
+      <div class="abv">Abv: {{beer.abv}}%</div>
+      <div class="ibu">Ibu: {{beer.ibu}}</div>
+      <!--<div class="description">Description: {{beer.description}}</div>-->
+    </div>`,
+    styles: [`
+      .name {
+        color: brown;
+      }
+    `]
+})
+export class BeerDetailComponent {
+  public beer: Beer;
+}
